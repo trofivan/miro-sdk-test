2347f802a1f2ec5a4591117133bbc5e6ed3d6fa4
Ivan Trofimov <ivan.tr@miro.com>
2022-04-10 13:47:42 +0400

    updated icon size

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const ROOT_PATH = 'https://trofivan.github.io/miro-sdk-test'
-const SVG_ICON = '<path d="M14.554,0C6.561,0,0,6.562,0,14.552c0,7.996,6.561,14.555,14.554,14.555c7.996,0,14.553-6.559,14.553-14.555 C29.106,6.562,22.55,0,14.554,0z"/>'
+const SVG_ICON = `
+	<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+		<circle cx="50%" cy="50%" r="12" fill="black"/>
+	</svg>
+`
 
 function run() {
 	miro.initialize({
